0453ae6f26b9be2e1bb208ae3a10196c459e57f2
Yann Hodiesne <yann.hodiesne@viacesi.fr>
2020-04-28 14:01:37 +0200

    Ajout du module d'index statique

---
+++ Lern-API/Modules/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Lern_API.Modules
+{
+    public sealed class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get("/", _ => Response.AsFile("Content/index.html", "text/html"));
+        }
+    }
+}
